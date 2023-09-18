18b830b768a9543d13583311aff616b578cbb206
Gavin Hatch <gavinrhatch@gmail.com>
2023-09-18 13:40:34 -0600

    poisson regression added

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -58,3 +58,43 @@ final$datetime <- as.character(format(final$datetime))
 
 # write to csv
 vroom_write(final, "bike_submission.csv", delim = ",")
+
+
+
+
+# Dr. Heaton Notes
+# Get predictions and test set and format for kaggle
+#lin_preds <- predict(bike_workflow, new_data = bikeTest) %>%
+  #bind_cols(.,bikeTest) %>% # bind predictions with test data
+  #select(datetime, .pred) %>% # Just keep datetime and predictions
+  #rename(count=.pred) %>% # rename pred to count (for submission to Kaggle)
+  #mutate(count=pmax(0,count)) %>% #pointwise max of (0,prediction)
+  #mutate(datetime=as.character(format(datetime))) #needed for right format
+
+
+
+######################
+# Poisson Regression #
+######################
+
+library(poissonreg)
+
+# create model
+pois_model <- poisson_reg() %>%
+  set_engine("glm") 
+
+# create workflow with recipe and model
+bike_pois_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(pois_model) %>%
+  fit(data = bikeTrain) # fit workflow
+
+bike_pois_pred <- predict(bike_pois_workflow, new_data=bikeTest) %>%
+  bind_cols(.,bikeTest) %>% # bind predictions with test data
+  select(datetime, .pred) %>% # Just keep datetime and predictions
+  rename(count = .pred) %>% # rename pred to count (for submission to Kaggle)
+  mutate(count = pmax(0,count)) %>% #pointwise max of (0,prediction)
+  mutate(count = ifelse(is.na(count), 0, count)) %>%
+  mutate(datetime=as.character(format(datetime))) #needed for right format
+
+vroom_write(bike_pois_pred, "bike_poisson_submission.csv", delim = ',')
